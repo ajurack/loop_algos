5c47b07b2d6d494c5ebbf2c5769d2e8e050739f6
Anne <anne.jurack@gmail.com>
2020-02-18 13:29:24 -0600

    finished

--- 4_extra_practice.js
+++ 4_extra_practice.js
@@ -1,5 +1,16 @@
 // Take your previous solutions and write them using the other kind of loop.  
 // I.E.  If you wrote both of your algorithms using for loops, now write them out with while loops
 
-
-// YOUR CODE HERE
+var i = 1;
+while (i <= 30) {
+    if ( i % 3 == 0 && i % 5 == 0 ) {
+        console.log("FizzBuzz");
+    } else if ( i % 3 == 0 ) {
+        console.log("Fizz");
+    } else if ( i % 5 == 0 ) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+    i++;
+}
